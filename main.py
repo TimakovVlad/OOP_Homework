faa9258891489916c16e7a2111d7cb34bf37594c
NetoLearn <vdtimakov00@gmail.com>
2023-10-30 19:38:22 +0300

    Complete third task

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -15,18 +15,82 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
+
+    def __average_grade(self):
+        ls_gr = list(self.grades.values())
+        if len(ls_gr) == 1:
+            av_gr = sum(ls_gr[0]) / len(ls_gr[0])
+            return round(av_gr, 1)
+        else:
+            av_gr_ls = []
+            for i in range(len(ls_gr)):
+                a = sum(ls_gr[i]) / len(ls_gr[i])
+                av_gr_ls.append(a)
+            av_gr = sum(av_gr_ls) / len(av_gr_ls)
+            return round(av_gr, 1)
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.__average_grade()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __lt__(self, other):
+        return self.__average_grade() < other.__average_grade()
+
+    def __le__(self, other):
+        return self.__average_grade() <= other.__average_grade()
+
+    def __gt__(self, other):
+        return self.__average_grade() > other.__average_grade()
+
+    def __ge__(self, other):
+        return self.__average_grade() >= other.__average_grade()
+
+    def __eq__(self, other):
+        return self.__average_grade() == other.__average_grade()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
-            
-class Lecturer(Mentor)
-    pass
 
-class  Reviewer(Mentor)
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def __average_grade(self):
+        ls_gr = list(self.grades.values())
+        if len(ls_gr) == 1:
+            av_gr = sum(ls_gr[0]) / len(ls_gr[0])
+            return round(av_gr, 1)
+        else:
+            av_gr_ls = []
+            for i in range(len(ls_gr)):
+                a = sum(ls_gr[i]) / len(ls_gr[i])
+                av_gr_ls.append(a)
+            av_gr = sum(av_gr_ls) / len(av_gr_ls)
+            return round(av_gr, 1)
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.__average_grade()}'
+
+    def __lt__(self, other):
+        return self.__average_grade() < other.__average_grade()
+
+    def __le__(self, other):
+        return self.__average_grade() <= other.__average_grade()
+
+    def __gt__(self, other):
+        return self.__average_grade() > other.__average_grade()
+
+    def __ge__(self, other):
+        return self.__average_grade() >= other.__average_grade()
+
+    def __eq__(self, other):
+        return self.__average_grade() == other.__average_grade()
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -36,15 +100,7 @@ class  Reviewer(Mentor)
         else:
             return 'Ошибка'
 
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+        
+        
