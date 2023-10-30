c46777b08bd2b6b026362633a059d4ff828af8d0
NetoLearn <vdtimakov00@gmail.com>
2023-10-30 19:36:23 +0300

    Complete second task

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+            
+class Lecturer(Mentor)
+    pass
+
+class  Reviewer(Mentor)
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,13 +35,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-            
-            
-class Lecturer(Mentor)
-    pass
-
-class  Reviewer(Mentor)
-    pass
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
